a5ff09045f68d44a362bde59c54149f5682acd23
Mahyar Darvishi <buddysirjava@outlook.com>
2025-11-26 12:51:48 +0330

    Add translation support: POT file and update script

---
+++ update-po.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir -p po
+xgettext --from-code=UTF-8 \
+  --add-comments \
+  --keyword=_ \
+  --keyword=N_ \
+  --package-name="Yet Another Radio" \
+  --output=po/yetanotherradio.pot \
+  yetanotherradio@io.github.buddysirjava/*.js \
+  yetanotherradio@io.github.buddysirjava/schemas/*.xml
+
+
+
+
+
