5b219a04081bcf3d170a9c83f12111f3ac4f4c07
Mahyar Darvishi <buddysirjava@outlook.com>
2025-12-08 20:33:43 +0330

    Added pack script

---
+++ pack.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gnome-extensions pack --podir=../po/ yetanotherradio@io.github.buddysirjava/ --extra-source=modules/ --extra-source=icons/ --extra-source=schemas/ --extra-source=radioUtils.js
